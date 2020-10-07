c28e3bd3a0ce33853581ea63b91f65e1094b40d3
saberscientist <70531349+saberscientist@users.noreply.github.com>
2020-10-07 12:28:51 +1100

    breaking changes for optimization

--- jaccard.js
+++ jaccard.js
@@ -1,4 +1,15 @@
-function* split2(val) { let _int = 0; while ((_int * 2) < val.length) { yield val.slice(_int * 2, (_int + 1) * 2); _int++; } };
+function splitter(str) {
+    let _int = 0;
+    const split = [];
+    for (let i = 0; i < str.length; i++) {
+        if (i + 2 <= str.length) {
+            const val = str.substring(i, i + 2);
+            split.push(val);
+        }
+    }
+    return split;
+}
+
 
 const unions = (_arr) => {
     const array = _arr.slice()
@@ -23,8 +34,8 @@ const intersections = (_arr) => {
 }
 
 const arrayCheck = (_arr) => {
-    for(x of _arr) {
-        if(!x || typeof x === "object" && this.strict) throw new Error("Could not parse an object."); else if(!x || typeof x === "object" && this.strict) return null;
+    for (x of _arr) {
+        if (!x || typeof x === "object" && this.strict) throw new Error("Could not parse an object."); else if (!x || typeof x === "object" && this.strict) return null;
     }
 }
 
@@ -32,17 +43,12 @@ const arrayCheck = (_arr) => {
 module.exports = class {
 
     constructor({ strict }) {
-        if(typeof strict !== "boolean") this.strict = true; else this.strict = strict;
+        if (typeof strict !== "boolean") this.strict = true; else this.strict = strict;
     }
 
-    compareString([str1, str2]) {      
-        if((typeof str1 === "string" || typeof str2 === "string") && this.strict) throw new Error("Cannot compare non-strings."); else if(typeof str1 === "string" || typeof str2 === "string" && !this.strict) return null;
-
-        const firstFused = Array.from(split2(str1.replace(/\s+/g, ''))).concat(Array.from(split2(str2.replace(/\s+/g, ''))));
-        const firstEval = intersections(firstFused) / unions(firstFused);
-
-        const secondFused = str1.replace(/\s+/g, '').split('').concat(str2.replace(/\s+/g, '').split(''));
-        const secondEval = intersections(secondFused) / unions(secondFused);
+    compareString([str1, str2]) {
+        if ((typeof str1 === "string" || typeof str2 === "string") && this.strict) throw new Error("Cannot compare non-strings."); else if (typeof str1 === "string" || typeof str2 === "string" && !this.strict) return null;
+        
 
         return (firstEval + secondEval) / 2;
     }
