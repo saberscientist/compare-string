9f50fcec17a25a603a9c915ed22bb7e06711d3eb
saberscientist <70531349+saberscientist@users.noreply.github.com>
2020-10-07 16:26:42 +1100

    add a new matchArray method, options as well

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # compare-string
 
-A lightweight, pure JS package to calculate the degree of similarity between strings. Uses a combination of the [Sørensen–Dice coefficient](https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient) and [Jaccard's Index](https://en.wikipedia.org/wiki/Jaccard_index)
+A lightweight, pure JS package to calculate the degree of similarity between strings. Uses a combination of the [Sørensen–Dice coefficient](https://en.wikipedia.org/wiki/S%C3%B8rensen%E2%80%93Dice_coefficient) and [Jaccard's Index](https://en.wikipedia.org/wiki/Jaccard_index). Can generally return faster, more accurate results than other edit-distance based algorithms, such as [Levenshtein](https://en.wikipedia.org/wiki/Levenshtein_distance).
 
+## installation
+`npm i compare-string`
 
+# usage
+```js
+const _stringCompare = require("compare-string")
+```
 
 
--- jaccard.js
+++ jaccard.js
@@ -1,4 +1,4 @@
-const splitter = (str) => {
+const tokenizer = (str) => {
     let _int = 0;
     const split = [];
     for (let i = 0; i < str.length; i++) {
@@ -13,48 +13,87 @@ const splitter = (str) => {
 
 const unions = (first, second) => {
     const val = [];
-   const _first = first.slice();
-   const _second = second.slice();
-   while(_first.length) {
-     const x = _first.shift();
-     if(!_second.includes(x) && !_first.includes(x)) val.push(x);
-   }
-    while(_second.length) {
-     const y = _second.shift();
-      if(!_second.includes(y) && !_first.includes(y)) val.push(y)
+    const _first = first.slice();
+    const _second = second.slice();
+    while (_first.length) {
+        const x = _first.shift();
+        if (!_second.includes(x) && !_first.includes(x)) val.push(x);
+    }
+    while (_second.length) {
+        const y = _second.shift();
+        if (!_second.includes(y) && !_first.includes(y)) val.push(y)
     };
     return val.length;
-  };
+};
+
 
 const intersections = (first, second) => {
     const value = [];
     const __first = first.slice()
     const __second = second.slice()
-    
-    while(__first.length) {
-     const x = __first.shift()
-     if(__second.includes(x)) value.push(x)
-    }
-     return value.length;
-   };
 
-const arrayCheck = (_arr) => {
-    for (x of _arr) {
-        if (!x || typeof x === "object" && this.strict) throw new Error("Could not parse an object."); else if (!x || typeof x === "object" && this.strict) return null;
+    while (__first.length) {
+        const x = __first.shift()
+        if (__second.includes(x)) value.push(x)
     }
+    return value.length;
+};
+
+const arrayCheck = (_arr, options) => {
+    if ((Array.isArray(!_arr) && options.strict) || !_arr) throw new Error("The parameter you provided must be an array with at least one element."); else if (Array.isArray(_arr) && !options.strict) return null;
+    for (const x of _arr) if (!x || typeof x !== "string" && options.strict) throw new Error("Array to find best match of can only contain strings."); else if (!x || typeof x !== "string" && !options.strict) return null;
 };
 
 
-module.exports = class {
+const typeGuard = {
+    strict: "boolean",
+}
+const defaultOptions = {
+    strict: true
+}
 
-    constructor({ strict }) {
-        if (typeof strict !== "boolean") this.strict = true; else this.strict = strict;
-    }
+//the actual interface
+module.exports = {
 
-    compareString(str1, str2) {
-        if ((typeof str1 !== "string" || typeof str2 !== "string") && this.strict) throw new Error("Cannot compare non-strings."); else if (typeof str1 !== "string" || typeof str2 !== "string" && !this.strict) return null;
-        const arr1 = splitter(str1.replace(/\s+/g, ''))
-        const arr2 = splitter(str2.replace(/\s+/g, ''))
-        return (intersections(arr1, arr2) / unions(arr1, arr2) + ((2 * intersections(arr1,arr2)) / (arr1.length + arr2.length))) / 2
+    compareString(str1, str2, options) {
+
+        if (!options) options = defaultOptions
+        for (const y of Object.keys(typeGuard)) {
+            if (typeGuard[y] !== typeof options[y]) options[y] = defaultOptions[y];
+        }
+
+        if ((typeof str1 !== "string" || typeof str2 !== "string") && options.strict) throw new Error("Cannot compare non-strings."); else if ((typeof str1 !== "string" || typeof str2 !== "string") && !strict) return null;
+        const arr1 = tokenizer(str1.replace(/\s+/g, ""));
+        const arr2 = tokenizer(str2.replace(/\s+/g, ""));
+        return (2 * intersections(arr1, arr2)) / (arr1.length + arr2.length)
+    },
+    //@returns String
+
+    matchArray(_str, array, options) {
+        if (!options) options = defaultOptions
+        for (const y of Object.keys(typeGuard)) {
+            if (typeGuard[y] !== typeof options[y]) options[y] = defaultOptions[y];
+        }
+        arrayCheck(array, options)
+        if (typeof _str !== "string" && options.strict) throw new Error("String to find best match of must be a string."); else if (typeof _str !== "string" && !options.strict) return null;
+
+        const ratings = [];
+        const str = _str.replace(/\s+/g, "");
+        for (let i = 0; i < array.length; i++) array[i] = array[i].replace(/\s+/g, "");
+
+        const arr1 = tokenizer(str)
+
+        for (x of array) {
+            const arr2 = tokenizer(x)
+            ratings.push([x, ((2 * intersections(arr1, arr2)) / (arr1.length + arr2.length))])
+        }
+        if (!ratings) return null;
+        ratings.sort(([, rating1], [, rating2]) => rating2 - rating1)
+        return {
+            bestMatch: ratings[0][0],
+            bestMatchRating: ratings[0][1],
+            sorted: ratings
+        }
     }
+    //@returns Object{sorted: Array[Array[name, rating]], bestMatch: String, bestMatchRating: Number}
 };
